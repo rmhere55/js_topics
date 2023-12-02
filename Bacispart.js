3451daa02b9c3dbe8be116c7cc159998cd3af1c4
raj laxmi singh <112322594+rmhere55@users.noreply.github.com>
2023-12-02 12:59:32 +0000

    basic concept of js

---
+++ Bacispart.js
@@ -0,0 +1,33 @@
+console.log("hyy")
+//  =======================Opreaters================================
+// console.log(9**9)  9 sqare 9
+// let b2
+// let c2
+// let a = b2 + c2
+// console.log(1 + 3 +"2")
+
+// // console.log(typeof nan)
+// console.log(2>1);
+// console.log(2==1);
+// console.log(2 < 1);
+
+// they make use confuse 
+// console.log(0 <= null); // ture  (comparestion conver in nulll in 0)
+
+
+//================ data type summary for  inter views=====================//
+// primitives 
+// 7 types : String , number , boolearn , null , undefined ,symbols , bigInt 
+// const score = false;
+// reference type : {}, [], ()
+// array , Objects , Functions 
+
+ const heros = ["Rm", "suga ", "jin"]
+ let  my= {
+    name :"hitashe",
+    age: 20,
+    add: "yahi"
+}
+const  f =  function (parameters) {
+    console.log("hello");
+}
