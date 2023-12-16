b8a9050314b218b3e0c88250544aa9454977481c
raj laxmi singh <112322594+rmhere55@users.noreply.github.com>
2023-12-16 14:02:47 +0000

    ho ja commit

--- fun.js
+++ fun.js
@@ -71,3 +71,69 @@ const add =(num,nu)=>({username:"Rm"})
   ( ( name)=>{
    console.log(`arrowfunction ${name}`);
   })("Rm");
+
+
+
+  // if 
+if (condition){
+
+}
+
+switch (key) {
+    case value:
+        
+        break;
+
+    default:
+        break;
+}
+//  turty or falsely 
+const useremail = "hitalshe.ai"
+
+if (useremail){
+    console.log("we got it");
+} else {
+    console.log("dont got it.");
+}
+
+// falsely
+// false , 0 , NaN , -0 , bigint 0n , "" , null , undefined 
+
+// turely 
+// "0" , "false" ,"" ,[] ,{} ,function (){}
+
+
+if (useremail.length === 0){
+    console.log("array is empty");
+}
+
+const emptypbj ={
+
+}
+if (Object.keys(emptypbj).length === 0)
+{
+    console.log(" object is empty");
+}
+
+
+//  console  par kam 
+// false == 0  :- true 
+// false = "" :- true
+// 0 ="" true
+
+
+// Nullish conescing operator (??) : null undefined
+let val1 
+// val1 = 5 ?? 10
+
+val1 = null ?? 10
+val1 = undefined ?? 15
+val1 = null ?? 10?? 20  
+
+console.log(val1);
+
+
+// terniary operater 
+//  condition ? true  :  false 
+const ice = 100;
+icetea >= 80 ?console.log("les then 8")  : console.log("more than 80");;
