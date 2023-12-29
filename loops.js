a27d7204047a7a0e8c3cf752f2e8984df003c0ac
raj laxmi singh <112322594+rmhere55@users.noreply.github.com>
2023-12-29 08:43:31 +0000

    for of

--- loops.js
+++ loops.js
@@ -78,3 +78,35 @@
 //  } while ( score <= 10);
 
 
+// // for of 
+// ["","",""]
+// [{},{},{}]
+
+const arr = [ 1,2,3, 4,5]
+for ( const num of arr) {
+    console.log(num);
+    
+}
+
+const greeting = "hello"
+for (const greet of greeting){
+    console.log(`each char is ${greet}`);
+}
+
+// const map = new Map()
+// map.set('IN', "india")
+// map.set('en', "england")
+// console.log(map);
+//  for (const [key,value] of  map){
+//     console.log( key , ':-' , value);
+//  } 
+
+//  const myobj ={
+//     'gm': 'hp',
+//     'gm': 'hyy'
+//  }
+
+//  for (const [key , value] of myobj) {
+    
+//     console.log( key , ':-' , value);
+//  }
